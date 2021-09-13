4d8919660659c60280c720ffb9490a2be1dbca21
Assis <assisguitar23@gmail.com>
2021-09-13 19:12:25 -0300

    first commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import type { AppProps } from 'next/app'
+import { Provider } from 'next-auth/client'
+
+import '../styles/globals.css'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <Provider session={pageProps.session}>
+      <Component {...pageProps} />
+    </Provider>
+  )
+}
+export default MyApp
