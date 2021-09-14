cde306e3a87faf3a34e1d9d40d6b2b813de4b299
Assis <assisguitar23@gmail.com>
2021-09-13 22:07:51 -0300

    pwa

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
+const withPWA = require('next-pwa')
+const withOffline = require('next-offline')
+
 module.exports = {
   reactStrictMode: true,
+  withPWA,
+  withOffline,
 }
