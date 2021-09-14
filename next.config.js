1462ab20aa5699904524c07d0e133f94103b18d1
Assis <assisguitar23@gmail.com>
2021-09-13 22:21:45 -0300

    fix pwa

--- next.config.js
+++ next.config.js
@@ -4,6 +4,40 @@ const withOffline = require('next-offline')
 
 module.exports = {
   reactStrictMode: true,
-  withPWA,
-  withOffline,
+  withPWA: () =>
+    withPWA({
+      pwa: {
+        dest: 'public',
+        register: true,
+        skipWaiting: true,
+        disable: process.env.NODE_ENV === 'development',
+        runtimeCaching: [
+          {
+            handler: 'networkFirst',
+            urlPattern: /^https?.*/,
+          },
+        ],
+      },
+    }),
+  withOffline: () =>
+    withOffline({
+      workboxOpts: {
+        swDest: 'public/service-worker.js',
+        runtimeCaching: [
+          {
+            handler: 'networkFirst',
+            options: {
+              cacheName: 'api-cache',
+              expiration: {
+                maxEntries: 200,
+                maxAgeSeconds: 60 * 60 * 24 * 30,
+              },
+              cacheableResponse: {
+                statuses: [0, 200],
+              },
+            },
+          },
+        ],
+      },
+    }),
 }
