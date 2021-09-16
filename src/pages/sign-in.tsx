b78e2fda2543eedcdd7a25ce041dfdffa6303c89
4ss1s <assisguitar23@gmail.com>
2021-09-15 23:25:37 -0300

    se o usuario nao confirmou, agora confirma

--- src/pages/sign-in.tsx
+++ src/pages/sign-in.tsx
@@ -1,6 +1,8 @@
+import React from 'react'
 import { GetServerSideProps, InferGetServerSidePropsType } from 'next'
 import { getSession, useSession } from 'next-auth/client'
-import React from 'react'
+
+import prisma from '@/lib/prisma'
 
 const SignIn = (
   props: InferGetServerSidePropsType<typeof getServerSideProps>
@@ -10,11 +12,36 @@ const SignIn = (
 
 export const getServerSideProps: GetServerSideProps = async ctx => {
   const session = await getSession(ctx)
+  const email = session?.user?.email
 
   if (!session) {
     return {
       redirect: {
         destination: '/',
+        permanent: false,
+      },
+    }
+  }
+
+  const user = await prisma.user.findUnique({
+    where: {
+      email: email || '',
+    },
+    select: {
+      id: true,
+    },
+  })
+
+  const verified = await prisma.userAccept.findMany({
+    where: {
+      userId: user?.id,
+    },
+  })
+
+  if (Object.keys(verified).length === 0) {
+    return {
+      redirect: {
+        destination: '/confirm-presence',
         permanent: false,
       },
     }
