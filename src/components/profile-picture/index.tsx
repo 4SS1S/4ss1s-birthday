ac88f7ddb21cc09853a3cb11a369f6a348886b77
4ss1s <assisguitar23@gmail.com>
2021-09-19 02:32:05 -0300

    feat: profile picture component

---
+++ src/components/profile-picture/index.tsx
@@ -0,0 +1,16 @@
+import { User } from 'next-auth'
+import React from 'react'
+
+export interface ProfilePictureProps {
+  user: User
+}
+
+export const ProfilePicture: React.FC<ProfilePictureProps> = ({ user }) => {
+  return (
+    <img
+      src={user.image || ''}
+      alt={user.name || ''}
+      className="rounded-full w-12 h-12"
+    />
+  )
+}
