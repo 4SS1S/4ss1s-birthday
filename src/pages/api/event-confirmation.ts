301651ee98c0dfb7e7c6d6a20c7dc9a77706b2fa
Assis <assisguitar23@gmail.com>
2021-09-22 19:41:43 -0300

    fix: change mind

--- src/pages/api/event-confirmation.ts
+++ src/pages/api/event-confirmation.ts
@@ -3,24 +3,24 @@ import { NextApiRequest, NextApiResponse } from 'next'
 import { EventConfirmationObject } from '../../classes'
 
 export default async function EventConfirmation(
-  req: NextApiRequest,
-  res: NextApiResponse
+	req: NextApiRequest,
+	res: NextApiResponse
 ) {
-  const eoc = new EventConfirmationObject(req, res)
+	const eoc = new EventConfirmationObject(req, res)
 
-  if (req.method === 'GET') {
-    eoc.list()
-  }
+	if (req.method === 'GET') {
+		eoc.list()
+	}
 
-  if (req.method === 'POST') {
-    eoc.create()
-  }
+	if (req.method === 'POST') {
+		eoc.create()
+	}
 
-  if (req.method === 'PUT') {
-    eoc.update()
-  }
+	if (req.method === 'PUT') {
+		eoc.update()
+	}
 
-  if (req.method === 'DELETE') {
-    eoc.delete()
-  }
+	if (req.method === 'DELETE') {
+		eoc.delete()
+	}
 }
