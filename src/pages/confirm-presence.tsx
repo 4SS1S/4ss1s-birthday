232e37be8d2d117b231f44ce64dd4532b4ac690f
Assis <assisguitar23@gmail.com>
2021-09-22 20:00:40 -0300

    feat: more changes on home

--- src/pages/confirm-presence.tsx
+++ src/pages/confirm-presence.tsx
@@ -12,171 +12,171 @@ import { Session } from 'next-auth'
 import { prisma } from '../lib'
 
 const ConfirmPresence = (
-  props: InferGetServerSidePropsType<typeof getServerSideProps>
+	props: InferGetServerSidePropsType<typeof getServerSideProps>
 ) => {
-  const session = props.data as Session
+	const session = props.data as Session
 
-  const router = useRouter()
+	const router = useRouter()
 
-  const { setLoading } = useContext(LoadingContext)
+	const { setLoading } = useContext(LoadingContext)
 
-  const handleCancel = () => {
-    setLoading(true)
+	const handleCancel = () => {
+		setLoading(true)
 
-    axios
-      .post('/api/event-confirmation', {
-        userId: props.user.id,
-        accepted: false,
-      })
-      .then(e => {
-        setLoading(false)
-        router.push('/cancel')
-      })
-      .catch(err => {
-        // TODO: create error page
-        console.log(err)
-        setLoading(false)
-      })
-  }
+		axios
+			.post('/api/event-confirmation', {
+				userId: props.user.id,
+				accepted: false,
+			})
+			.then(e => {
+				setLoading(false)
+				router.push('/cancel')
+			})
+			.catch(err => {
+				// TODO: create error page
+				console.log(err)
+				setLoading(false)
+			})
+	}
 
-  const handleConfirm = () => {
-    axios
-      .post('/api/event-confirmation', {
-        userId: props.user.id,
-        accepted: true,
-      })
-      .then(e => {
-        router.push('/home')
-      })
-      .catch(err => {
-        console.log(err)
-      })
-  }
+	const handleConfirm = () => {
+		axios
+			.post('/api/event-confirmation', {
+				userId: props.user.id,
+				accepted: true,
+			})
+			.then(e => {
+				router.push('/home')
+			})
+			.catch(err => {
+				console.log(err)
+			})
+	}
 
-  return (
-    <div className="flex w-screen justify-center flex-col items-center">
-      {/* <div className="overflow-hidden rounded-full shadow-lg">
+	return (
+		<div className="flex w-screen justify-center flex-col items-center">
+			{/* <div className="overflow-hidden rounded-full shadow-lg">
         <img src={session.user?.image || ''} alt={session.user?.name || ''} />
       </div> */}
-      <div className="mt-4">
-        <DialogBox>
-          <strong className="tracking-wide">
-            Você {session.user?.name}, confirma presença no dia{' '}
-            {props.event.date}/{props.event.month} (sexta-feira) às{' '}
-            {props.event.time}:{props.event.minutes}h no {props.event.location}?
-          </strong>
+			<div className="mt-4">
+				<DialogBox>
+					<strong className="tracking-wide">
+						Você {session.user?.name}, confirma presença no dia{' '}
+						{props.event.date}/{props.event.month} (sexta-feira) às{' '}
+						{props.event.time}:{props.event.minutes}h no {props.event.location}?
+					</strong>
 
-          <div className="font-thin mt-6 mb-1">
-            Ao confirmar estar no local, você estará aceitando também:
-          </div>
-          <ul className="font-light space-y-2">
-            <li className="flex space-x-1">
-              <span className="text-green-500 font-semibold text-2xl">
-                <MdDone />
-              </span>{' '}
-              <span>Boa Comida</span>
-            </li>
-            <li className="flex space-x-1">
-              <span className="text-green-500 font-semibold text-2xl">
-                <MdDone />
-              </span>{' '}
-              <span>Ótimas cervejas</span>
-            </li>
-            <li className="flex space-x-1">
-              <span className="text-green-500 font-semibold text-2xl">
-                <MdDone />
-              </span>{' '}
-              <span>Ambiente agradável e descontraido</span>
-            </li>
-            <li className="flex space-x-1">
-              <span className="text-green-500 font-semibold text-2xl">
-                <MdDone />
-              </span>{' '}
-              <span>Sua presença é um grande presente ❤️</span>
-            </li>
-            <li className="flex space-x-2 items-center">
-              <span className="text-red-500 font-semibold text-xl">
-                <VscError />
-              </span>{' '}
-              <span>Não precisa levar presente</span>
-            </li>
-            <li className="flex space-x-2 items-center">
-              <span className="text-red-500 font-semibold text-xl">
-                <VscError />
-              </span>{' '}
-              <span>Não precisa gastar no local</span>
-            </li>
-          </ul>
+					<div className="font-thin mt-6 mb-1">
+						Ao confirmar estar no local, você estará aceitando também:
+					</div>
+					<ul className="font-light space-y-2">
+						<li className="flex space-x-1">
+							<span className="text-green-500 font-semibold text-2xl">
+								<MdDone />
+							</span>{' '}
+							<span>Boa Comida</span>
+						</li>
+						<li className="flex space-x-1">
+							<span className="text-green-500 font-semibold text-2xl">
+								<MdDone />
+							</span>{' '}
+							<span>Ótimas cervejas</span>
+						</li>
+						<li className="flex space-x-1">
+							<span className="text-green-500 font-semibold text-2xl">
+								<MdDone />
+							</span>{' '}
+							<span>Ambiente agradável e descontraido</span>
+						</li>
+						<li className="flex space-x-1">
+							<span className="text-green-500 font-semibold text-2xl">
+								<MdDone />
+							</span>{' '}
+							<span>Sua presença é um grande presente ❤️</span>
+						</li>
+						<li className="flex space-x-2 items-center">
+							<span className="text-red-500 font-semibold text-xl">
+								<VscError />
+							</span>{' '}
+							<span>Não precisa levar presente</span>
+						</li>
+						<li className="flex space-x-2 items-center">
+							<span className="text-red-500 font-semibold text-xl">
+								<VscError />
+							</span>{' '}
+							<span>Não precisa gastar no local</span>
+						</li>
+					</ul>
 
-          <div className="relative mt-6 h-7 overflow-hidden" />
+					<div className="relative mt-6 h-7 overflow-hidden" />
 
-          <div className="w-full flex mt-5 left-0 absolute bottom-0 overflow-hidden">
-            <div className="absolute top-0 w-screen -left-12 h-px bg-gray-300 bg-opacity-50" />
-            <button
-              className="w-full border-r-2 border-opacity-50 border-gray-300 p-4 font-light"
-              onClick={handleCancel}
-            >
-              Cancelar
-            </button>
-            <button className="w-full p-4 font-medium" onClick={handleConfirm}>
-              Confirmar
-            </button>
-          </div>
-        </DialogBox>
-      </div>
-    </div>
-  )
+					<div className="w-full flex mt-5 left-0 absolute bottom-0 overflow-hidden">
+						<div className="absolute top-0 w-screen -left-12 h-px bg-gray-300 bg-opacity-50" />
+						<button
+							className="w-full border-r-2 border-opacity-50 border-gray-300 p-4 font-light"
+							onClick={handleCancel}
+						>
+							Cancelar
+						</button>
+						<button className="w-full p-4 font-medium" onClick={handleConfirm}>
+							Confirmar
+						</button>
+					</div>
+				</DialogBox>
+			</div>
+		</div>
+	)
 }
 
 export const getServerSideProps: GetServerSideProps = async ctx => {
-  const session = await getSession(ctx)
+	const session = await getSession(ctx)
 
-  const event = await prisma.event?.findFirst()
-  const user = await prisma.user?.findUnique({
-    where: {
-      email: session?.user?.email || '',
-    },
-  })
+	const event = await prisma.event?.findFirst()
+	const user = await prisma.user?.findUnique({
+		where: {
+			email: session?.user?.email || '',
+		},
+	})
 
-  if (!session) {
-    return {
-      redirect: {
-        destination: '/',
-        permanent: false,
-      },
-    }
-  }
+	if (!session) {
+		return {
+			redirect: {
+				destination: '/',
+				permanent: false,
+			},
+		}
+	}
 
-  // if (!event) {
-  //   event = {
-  //     id: 0,
-  //     title: '',
-  //     description: '',
-  //     location: '4Beer Zona Sul',
-  //     end: new Date(),
-  //     start: new Date('2021-10-01:16:00:00'),
-  //     createdAt: new Date(),
-  //     updatedAt: new Date(),
-  //     latitude: 0,
-  //     longitude: 0,
-  //   }
-  // }
+	// if (!event) {
+	//   event = {
+	//     id: 0,
+	//     title: '',
+	//     description: '',
+	//     location: '4Beer Zona Sul',
+	//     end: new Date(),
+	//     start: new Date('2021-10-01:16:00:00'),
+	//     createdAt: new Date(),
+	//     updatedAt: new Date(),
+	//     latitude: 0,
+	//     longitude: 0,
+	//   }
+	// }
 
-  return {
-    props: {
-      data: session,
-      user: {
-        id: user?.id || 0,
-      },
-      event: {
-        location: event?.location,
-        date: `${new Date(event?.start as Date).getDate()}`,
-        month: `${new Date(event?.start as Date).getMonth() + 1}`,
-        time: `${new Date(event?.start as Date).getHours() + 3}`,
-        minutes: `${new Date(event?.start as Date).getMinutes()}`,
-      },
-    },
-  }
+	return {
+		props: {
+			data: session,
+			user: {
+				id: user?.id || 0,
+			},
+			event: {
+				location: event?.location,
+				date: `${new Date(event?.start as Date).getDate()}`,
+				month: `${new Date(event?.start as Date).getMonth() + 1}`,
+				time: `${new Date(event?.start as Date).getHours() + 3}`,
+				minutes: `${new Date(event?.start as Date).getMinutes()}`,
+			},
+		},
+	}
 }
 
 export default ConfirmPresence
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -5,9 +5,22 @@ import { GetServerSideProps, InferGetServerSidePropsType } from 'next'
 import OneSignal from 'react-onesignal'
 
 import { ProfilePicture, DialogBox } from '@/components'
+import { prisma } from '../lib'
+
+interface HomeProps {
+	session: Session
+	event: {
+		location: string
+		date: string
+		month: string
+		time: string
+		minutes: string
+	}
+	confirmedUsers
+}
 
 // eslint-disable-next-line prettier/prettier
-const Home = ({ session }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+const Home = ({ session, event,confirmedUsers }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
 	const [initialized, setInitialized] = useState(false)
 
 	useEffect(() => {
@@ -38,8 +51,32 @@ const Home = ({ session }: InferGetServerSidePropsType<typeof getServerSideProps
 					>
 						<ProfilePicture user={session.user as Session} />
 					</div>
-					<div className="mt-8">
-						<h1>Olá {session.user?.name || ''} você está confirmado</h1>
+					<div className="mt-12">
+						Olá {session.user?.name || ''} você está confirmado no dia{' '}
+						{event.date}/{event.month} às {event.time}:{event.minutes} em{' '}
+						{event.location}. Aqui tem um link para o{' '}
+						<a
+							className="underline text-blue-500"
+							href="https://www.google.com/maps/place/4Beer+Zona+Sul+-+Cerveja+e+Cultura/@-30.1068587,-51.2541419,15z/data=!4m5!3m4!1s0x0:0x892df2b5eec7b711!8m2!3d-30.1068587!4d-51.2541419"
+						>
+							Google Maps
+						</a>{' '}
+						e um link{' '}
+						<a
+							className="underline text-blue-500"
+							href="https://loja.4beerpoa.com.br/"
+						>
+							oficial do estabelicimento
+						</a>
+						. Caso queira também pode ligar para o telefone{' '}
+						<a
+							className="underline text-blue-500"
+							href="tel:+55(51) 3377-4024"
+							type="tel"
+						>
+							3377-4024
+						</a>
+						.<p>Até agora são {confirmedUsers} pessoas confirmadas!!!</p>
 					</div>
 				</DialogBox>
 			</div>
@@ -49,6 +86,14 @@ const Home = ({ session }: InferGetServerSidePropsType<typeof getServerSideProps
 
 export const getServerSideProps: GetServerSideProps = async context => {
 	const session = await getSession(context)
+
+	const event = await prisma.event?.findFirst()
+
+	const users = await prisma.userConfirmation.findMany({
+		where: {
+			accepted: true,
+		},
+	})
 
 	if (!session) {
 		return {
@@ -62,6 +107,14 @@ export const getServerSideProps: GetServerSideProps = async context => {
 	return {
 		props: {
 			session,
+			event: {
+				location: event?.location,
+				date: `${new Date(event?.start as Date).getDate()}`,
+				month: `${new Date(event?.start as Date).getMonth() + 1}`,
+				time: `${new Date(event?.start as Date).getHours() + 3}`,
+				minutes: `${new Date(event?.start as Date).getMinutes()}`,
+			},
+			confirmedUsers: users.length - 1,
 		},
 	}
 }
