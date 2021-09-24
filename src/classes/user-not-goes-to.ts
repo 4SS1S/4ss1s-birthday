207d9d954d26a3a85ada3a27662e6f77095d2886
4ss1s <assisguitar23@gmail.com>
2021-09-24 01:03:27 -0300

    fix: user not goes

--- src/classes/user-not-goes-to.ts
+++ src/classes/user-not-goes-to.ts
@@ -1,37 +1,32 @@
 import { BirthdayObject } from './birthday-object'
 
 export class UserNotGoesToObject extends BirthdayObject {
-	async list() {
-		const data = await this.getPrisma().userNotGoesTo.findMany()
-
-		return this.getRes().status(200).json({
-			data,
-		})
-	}
-
-	async create() {
-		const user = await this.getUserInfo()
-		const body = this.getBody()
-
-		if (!user) {
-			return this.getRes().status(401).json({
-				error: 'Unauthorized',
-			})
-		}
-
-		const data = await this.getPrisma().userNotGoesTo.create({
-			data: {
-				user: {
-					connect: {
-						id: user.id,
-					},
-				},
-				...body,
-			},
-		})
-
-		return this.getRes().status(200).json({
-			data,
-		})
-	}
+	// async list() {
+	// 	const data = await this.getPrisma().userNotGoesTo.findMany()
+	// 	return this.getRes().status(200).json({
+	// 		data,
+	// 	})
+	// }
+	// async create() {
+	// 	const user = await this.getUserInfo()
+	// 	const body = this.getBody()
+	// 	if (!user) {
+	// 		return this.getRes().status(401).json({
+	// 			error: 'Unauthorized',
+	// 		})
+	// 	}
+	// 	const data = await this.getPrisma().userNotGoesTo.create({
+	// 		data: {
+	// 			user: {
+	// 				connect: {
+	// 					id: user.id,
+	// 				},
+	// 			},
+	// 			...body,
+	// 		},
+	// 	})
+	// 	return this.getRes().status(200).json({
+	// 		data,
+	// 	})
+	// }
 }
