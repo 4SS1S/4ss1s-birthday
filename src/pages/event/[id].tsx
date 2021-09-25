8e2fd2642fcb7495f9856e92d41707e6f17952a0
Assis <assisguitar23@gmail.com>
2021-09-24 23:00:28 -0300

    feat: event page

--- src/pages/event/[id].tsx
+++ src/pages/event/[id].tsx
@@ -1,10 +1,65 @@
-import { GetStaticProps, InferGetStaticPropsType } from 'next'
-import React from 'react'
+import React, { useEffect } from 'react'
+import { GetStaticPaths, GetStaticProps } from 'next'
 
-const Event = props => {
+import prisma from '@/lib/prisma'
+
+interface UserProps {
+	id: number
+	name: string
+	image: string
+	accepted: boolean
+}
+
+interface EventProps {
+	id: number
+	event: {
+		id: number
+		title: string
+		description: string
+		date: string
+	}
+	users: UserProps[]
+}
+
+const Event = ({ id, event, users }: EventProps) => {
+	useEffect(() => {
+		console.log(users)
+	}, [])
+
 	return (
 		<div>
-			<h1>Event</h1>
+			<div className="text-center">
+				<h1 className="text-3xl font-semibold font-mono">
+					Sobre o evento no {event.title}
+				</h1>
+			</div>
+			{/* <div className="flex flex-col items-center">
+				<div className="flex flex-col items-center">
+					<img
+						// src={event.image}
+						alt={event.title}
+						className="rounded-full h-32 w-32"
+					/>
+					<h1 className="text-3xl font-semibold font-mono">{event.title}</h1>
+				</div>
+				<p className="text-xl">{event.description}</p>
+			</div> */}
+			<div className="flex flex-col items-center">
+				<h1 className="text-3xl font-semibold font-mono">Participantes</h1>
+				<div className="flex flex-wrap items-center">
+					{users
+						.filter(i => i.accepted === true)
+						.map(user => (
+							<div key={user.id} className="flex flex-col items-center">
+								<img
+									src={user.image}
+									alt={user.name}
+									className="rounded-full h-12 w-12"
+								/>
+							</div>
+						))}
+				</div>
+			</div>
 		</div>
 	)
 }
@@ -12,10 +67,62 @@ const Event = props => {
 export const getStaticProps: GetStaticProps = async context => {
 	const id = context.params?.id
 
+	const event = await prisma.event.findUnique({
+		where: {
+			id: parseInt(id as string),
+		},
+	})
+
+	const users = await prisma.userConfirmation.findMany({
+		include: {
+			user: {
+				select: {
+					name: true,
+					image: true,
+				},
+			},
+		},
+	})
+
+	if (!event) {
+		return {
+			props: {
+				event: null,
+			},
+		}
+	}
+
+	if (!users) {
+		return {
+			props: {
+				event: null,
+			},
+		}
+	}
+
 	return {
 		props: {
 			id,
+			event: {
+				id: event.id,
+				title: event.title,
+				description: event.description,
+				date: new Date(event.start).toTimeString(),
+			},
+			users: users.map(user => ({
+				id: user.id,
+				name: user.user.name,
+				image: user.user.image,
+				accepted: user.accepted,
+			})),
 		},
+	}
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+	return {
+		paths: [{ params: { id: '1' } }],
+		fallback: false,
 	}
 }
 
